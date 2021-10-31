9e9fb2f99b201cd0ffa01a50014e986e82ddb244
srinadh1304 <srinadh1304@gmail.com>
2021-10-31 17:08:54 +0530

    ADDED: function to pass name as parameter

--- src/main/java/com/example/springrestdemo/controller/HelloWorldController.java
+++ src/main/java/com/example/springrestdemo/controller/HelloWorldController.java
@@ -2,6 +2,8 @@ package com.example.springrestdemo.controller;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -14,6 +16,11 @@ public class HelloWorldController
     public String sayHello()
     {
         return "Hello from Bridgelabz!";
+    }
+    @RequestMapping(value="/query", method=RequestMethod.GET)
+    public String sayHello(@RequestParam(value="name") String name) 
+    {
+        return "Hello "+name;
     }
 
 
