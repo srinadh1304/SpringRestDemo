f2ac6769ff9dbd11d3b5d4dcd91dbb9056d4e7e9
srinadh1304 <srinadh1304@gmail.com>
2021-10-31 17:03:16 +0530

    ADDED: class to demonstrate http get request method

---
+++ src/main/java/com/example/springrestdemo/controller/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.example.springrestdemo.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldController 
+{
+    @RequestMapping(value = {"","/","/home"})
+    public String sayHello()
+    {
+        return "Hello from Bridgelabz!";
+    }
+
+
+}
